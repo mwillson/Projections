b2da34c9909725238c469451d13ba87ea17bb5c3
Mark Willson <mwillson@oswego.edu>
2012-12-20 17:08:48 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projections::Application.config.secret_token = 'b594802838156124cfb4bc320f9103ce5adcad5dea4d20d0648e101ccc06e9ef9e39fe858dbf5623e4859b7ce741c967c1b2bd8b0e2d0312fe50aa98cc1d6069'
