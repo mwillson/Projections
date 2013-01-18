244a221bd9989ea38be744c3c08aa25b3da51c61
Mark Willson <mwillson@oswego.edu>
2013-01-18 15:53:26 -0500

    authorization and editing of users, and index of all for signed in users

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -45,4 +45,64 @@ describe "User pages" do
     it { should have_selector('title', text: user.name) }
   end # Show page
 
+  describe "edit page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    it { should have_selector('h1', text: "Update your profile") }
+    it { should have_selector('title', text: "Edit user") }
+    it { should have_link('change', href: 'http://gravatar.com/emails') }
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name", with: new_name
+        fill_in "Email", with: new_email
+        fill_in "Password", with: user.password
+        fill_in "Confirmation", with: user.password
+        click_button "Save changes"
+      end
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+    end
+  end # Edit page
+
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each)do
+      sign_in user
+      visit users_path
+    end
+    
+    it { should have_selector('title', text: 'All users') }
+    it { should have_selector('h1', text: 'All users') }
+    
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all) { User.delete_all }
+      
+      it { should have_selector('div.pagination') }
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+
+  end # index page
+
 end
